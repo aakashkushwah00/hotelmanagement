c4983999676d55a97f2e2c725fcb9dced84b5957
aakashkushwah00 <aakash.kushwah@codiatic.com>
2024-01-06 16:41:18 +0530

    first commit for normal login and logout functionality

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about 
+  end
+
+  def room
+  end
+
+  def blog 
+  end 
+
+  def contact 
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  root 'home#index'
+
+  get '/about', to: 'home#about'
+  get '/room', to: 'home#room'
+  get '/blog', to: 'home#blog'
+  get '/contact', to: 'home#contact'
+
+  
+end

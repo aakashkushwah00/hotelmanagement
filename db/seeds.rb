127d1aef30836ab345e6c8d412727ee9da7c8081
aakashkushwah00 <aakash.kushwah@codiatic.com>
2024-01-23 16:41:56 +0530

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user = User.create!(email: 'aakashkushwah9012@gmail.com', password: '1234567', password_confirmation: '1234567')
+# user = User.create!(email: 'aakashkushwah9012@gmail.com', password: '1234567', password_confirmation: '1234567')
 
-user.add_role :admin
+# user.add_role :admin
 
 
